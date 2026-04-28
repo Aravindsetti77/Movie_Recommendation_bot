247f520b4b966b670075905a4b50e6719cbcc0cc
Aravindsetti77 <aravindsetti77@gmail.com>
2026-04-28 18:48:13 +0530

    Verified the Embeddings and improved Code Readability

--- src/embed.py
+++ src/embed.py
@@ -10,14 +10,22 @@ emb_fn=embedding_functions.SentenceTransformerEmbeddingFunction(
     device="cuda")
 collection=client.get_or_create_collection(name="TMDB",embedding_function=emb_fn,metadata={"hnsw:space": "cosine"})
 df=pd.read_csv("Data/TMDB_clear_data.csv")
-batch_size=512
-rows=len(df)
-for i in range(0,rows,batch_size):
-    batch_df=df.iloc[i:i+batch_size]
-    collection.add(
-        documents=batch_df['dna'].tolist(),
-        metadatas=[{"title": row['title'], "genres": row['genres_list']} for _, row in batch_df.iterrows()],
-        ids=batch_df['id'].astype(str).tolist()
+def create_embeddings():
+    batch_size=512
+    rows=len(df)
+    for i in range(0,rows,batch_size):
+        batch_df=df.iloc[i:i+batch_size]
+        collection.add(
+            documents=batch_df['dna'].tolist(),
+            metadatas=[{"title": row['title'], "genres": row['genres_list']} for _, row in batch_df.iterrows()],
+            ids=batch_df['id'].astype(str).tolist()
+        )
+        if i%512==0:
+            print(f"processed{i}/{rows}")
+def verify_embedding(id):
+    result=collection.get(
+        ids=[id],
+        include=["embeddings", "metadatas", "documents"]
     )
-    if i%512==0:
-        print(f"processed{i}/{rows}")
+    print(result['embeddings'][0])
+verify_embedding("27205")
