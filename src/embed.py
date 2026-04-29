1c2c27604b3580e19c304cdf44be3997d6a9a802
Aravindsetti77 <aravindsetti77@gmail.com>
2026-04-29 09:05:22 +0530

    Improved code reusability

--- src/embed.py
+++ src/embed.py
@@ -1,31 +1,32 @@
 import pandas as pd
 import numpy as np
 import chromadb
+import torch
 from chromadb.utils import embedding_functions
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 from sentence_transformers import SentenceTransformer
-client=chromadb.PersistentClient(path="./movie_db")
-model="all-MiniLM-L6-V2"
-emb_fn=embedding_functions.SentenceTransformerEmbeddingFunction(
-    model_name=model,
-    device="cuda")
-collection=client.get_or_create_collection(name="TMDB",embedding_function=emb_fn,metadata={"hnsw:space": "cosine"})
-df=pd.read_csv("Data/TMDB_clear_data.csv")
-def create_embeddings():
-    batch_size=512
-    rows=len(df)
-    for i in range(0,rows,batch_size):
-        batch_df=df.iloc[i:i+batch_size]
-        collection.add(
-            documents=batch_df['dna'].tolist(),
-            metadatas=[{"title": row['title'], "genres": row['genres_list']} for _, row in batch_df.iterrows()],
-            ids=batch_df['id'].astype(str).tolist()
+class Embeddings:
+    def __init__(self,client):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = SentenceTransformer('All-MiniLM-L6-v2', device=self.device)
+        self.emb_fn=SentenceTransformerEmbeddingFunction(model_name="all_MiniLM-L6-V2",device="cuda")
+        self.collectioncollection=client.get_or_create_collection(name="TMDB",embedding_function=self.emb_fn,metadata={"hnsw:space": "cosine"})
+        self.df=pd.read_csv("Data/TMDB_clear_data.csv")
+    def create_embeddings(self):
+        batch_size=512
+        rows=len(self.df)
+        for i in range(0,rows,batch_size):
+            batch_df=self.df.iloc[i:i+batch_size]
+            self.collection.add(
+                documents=batch_df['dna'].tolist(),
+                metadatas=[{"title": row['title'], "genres": row['genres_list']} for _, row in batch_df.iterrows()],
+                ids=batch_df['id'].astype(str).tolist()
+            )
+            if i%512==0:
+                print(f"processed{i}/{rows}")
+    def verify_embedding(self,id):
+        result=self.collection.get(
+            ids=[str(id)],
+            include=["embeddings", "metadatas", "documents"]
         )
-        if i%512==0:
-            print(f"processed{i}/{rows}")
-def verify_embedding(id):
-    result=collection.get(
-        ids=[id],
-        include=["embeddings", "metadatas", "documents"]
-    )
-    print(result['embeddings'][0])
-verify_embedding("27205")
+        print(result['embeddings'][0])
